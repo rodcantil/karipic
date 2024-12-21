95a68294a4e823e1e24df76fe716ebbf5d7a9e23
rodcantil <rodolfo.cantillana@gmail.com>
2024-12-21 06:42:39 -0300

    4to finalizando

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
@@ -10,8 +12,6 @@ class ApplicationController < ActionController::Base
 
   private
   def authorize_request(kind = nil)
-    unless kind.include?(current_user.role)
     redirect_to posts_path, notice: "No estas permitido a realizar esta accion."
-    end
-end
+  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :phone, :image ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :image ])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :phone, :image ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :image ])
   end
 
   # The path used after sign up.
